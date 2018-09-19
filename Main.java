e9c41e3de1b06e8ef9253a6d79f40287d9d09dfa
Zach <innesza777@gmail.com>
2018-09-18 21:36:12 -0400

    Committed to make sure that evrything is up to date

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Transitions {
+
+}
+
+
+class State {
+    boolean isAccepting;
+    Transitions transitions;
+    public State() {
+
+    }
+}
+
+class DFA {
+    char[] alphabet;
+    State start;
+    public DFA() {
+        start = new State();
+    }
+}
+
+public class Main {
+    public static void main (String[] args){
+        
+    }
+}
