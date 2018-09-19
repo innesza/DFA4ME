07f829b7dbd85d8f71dc966566d3310a74b20505
Zucc <innesza777@gmail.com>
2018-09-19 01:19:03 -0400

    Added command line input of file, as well as beginning work on DFA class

--- Main.java
+++ Main.java
@@ -1,26 +1,52 @@
-class Transitions {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
 
+class Transition {
+    String symbol;
+    State tranState;
+    public Transition (String s, State t){
+        symbol = s;
+        tranState = t;
+    }
 }
 
 
 class State {
     boolean isAccepting;
-    Transitions transitions;
-    public State() {
-
+    Transition[] transitions;
+    public State(int n) { //n is size of alphabet
+        transitions = new Transition[n];
     }
 }
 
 class DFA {
-    char[] alphabet;
-    State start;
-    public DFA() {
-        start = new State();
+    String[] alphabet;
+    State[] states;
+    public DFA(int n, String[] a) { //n is number of states, a is the alphabet
+        alphabet = a;
+        states = new State[n];
+    }
+
+
+    public boolean TestString () {
+        return false;
     }
 }
 
 public class Main {
     public static void main (String[] args){
-        
+        File file = new File(args[0]);
+        DFA dfa;
+        try {
+            FileInputStream in = new FileInputStream(file);
+            Scanner s = new Scanner(in);
+            String[] tempAlpha = s.nextLine().split(","); //temporarily holds the alphabet for the DFA
+            int nStates = s.nextInt();
+            s.nextLine(); //Stops java from getting confused
+            dfa = new DFA(nStates, tempAlpha); //Creates the dfa
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
